5a468406044157ed50e46f3768868999dc0fee60
Salam Zuriqi <Salamalzuriqi@hotmail.com>
2024-08-15 17:21:47 +0300

    DB Configuration

---
+++ Models/CandidateConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+namespace SigmaApplication.Models
+{
+    public class CandidateConfiguration : IEntityTypeConfiguration<Candidate>
+    {
+        public void Configure(EntityTypeBuilder<Candidate> builder)
+        {
+            builder.ToTable("Candidate");
+            builder.Property(x => x.Id).ValueGeneratedOnAdd();
+        }
+    }
+}
